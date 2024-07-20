49af2061f8e7ac87b556d918f771c52b6b83517e
Valeri Sokolov <ulfurinn@ulfurinn.net>
2024-07-21 01:17:58 +0200

    skip some plugs

--- lib/blogpub_web/endpoint.ex
+++ lib/blogpub_web/endpoint.ex
@@ -4,26 +4,26 @@ defmodule BlogpubWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  @session_options [
-    store: :cookie,
-    key: "_blogpub_key",
-    signing_salt: "cVdM9HYi",
-    same_site: "Lax"
-  ]
+  # @session_options [
+  #   store: :cookie,
+  #   key: "_blogpub_key",
+  #   signing_salt: "cVdM9HYi",
+  #   same_site: "Lax"
+  # ]
 
-  socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]],
-    longpoll: [connect_info: [session: @session_options]]
+  # socket "/live", Phoenix.LiveView.Socket,
+  #   websocket: [connect_info: [session: @session_options]],
+  #   longpoll: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :blogpub,
-    gzip: false,
-    only: BlogpubWeb.static_paths()
+  # plug Plug.Static,
+  #   at: "/",
+  #   from: :blogpub,
+  #   gzip: false,
+  #   only: BlogpubWeb.static_paths()
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
@@ -32,9 +32,9 @@ defmodule BlogpubWeb.Endpoint do
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :blogpub
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
+  # plug Phoenix.LiveDashboard.RequestLogger,
+  #   param_key: "request_logger",
+  #   cookie_key: "request_logger"
 
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
@@ -45,8 +45,7 @@ defmodule BlogpubWeb.Endpoint do
     body_reader: {BlogpubWeb.CachingReader, :read_body, []},
     json_decoder: Phoenix.json_library()
 
-  plug Plug.MethodOverride
+  # plug Plug.MethodOverride
   plug Plug.Head
-  plug Plug.Session, @session_options
   plug BlogpubWeb.Router
 end
