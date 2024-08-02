c40ab785f73bd258b3a656ccd8a49b735bc5404a
Valeri Sokolov <ulfurinn@ulfurinn.net>
2024-08-02 21:48:33 +0200

    remove unused routes

--- lib/blogpub_web/router.ex
+++ lib/blogpub_web/router.ex
@@ -21,21 +21,4 @@ defmodule BlogpubWeb.Router do
 
     get "/*path", APub.Controller, :object
   end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:blogpub, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: BlogpubWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
 end
